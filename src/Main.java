cf306475f63bddf048dcac64a0deacec3a458c66
valdirapn <valdiraires.pn@gmail.com>
2022-12-29 21:29:07 -0300

    final

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,6 @@
+import br.com.dio.desafio.dominio.Bootcamp;
 import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Dev;
 import br.com.dio.desafio.dominio.Mentoria;
 
 import java.time.LocalDate;
@@ -24,9 +26,44 @@ public class Main {
         mentoria.setDescricao("descrição mentoria java");
         mentoria.setData(LocalDate.now());
 
-        System.out.println(curso1);
-        System.out.println(curso2);
-        System.out.println(mentoria);
+//        System.out.println(curso1);
+//        System.out.println(curso2);
+//        System.out.println(mentoria);
+
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Bootcamp Java Developer");
+        bootcamp.setDescricao("Descrição Bootcamp Java Developer");
+        bootcamp.getConteudos().add(curso1);
+        bootcamp.getConteudos().add(curso2);
+        bootcamp.getConteudos().add(mentoria);
+
+        Dev devValdir = new Dev();
+        devValdir.setNome("Valdir");
+        devValdir.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos Inscritos " + devValdir.getNome() + ": " + devValdir.getConteudosInscritos());
+
+        System.out.println("-------------------");
+
+        devValdir.progredir();
+        devValdir.progredir();
+        System.out.println("Conteúdos Inscritos " + devValdir.getNome() + ": " + devValdir.getConteudosInscritos());
+        System.out.println("Conteúdos Concluídos " + devValdir.getNome() + ": " + devValdir.getConteudosConcluidos());
+        System.out.println("XP: " + devValdir.calcularTotalXp());
+
+        System.out.println("-------------------");
+
+        Dev devCarlos = new Dev();
+        devCarlos.setNome("Carlos");
+        devCarlos.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos Inscritos " + devCarlos.getNome() + ": " + devCarlos.getConteudosInscritos());
+
+        System.out.println("-------------------");
+        devCarlos.progredir();
+        devCarlos.progredir();
+        devCarlos.progredir();
+        System.out.println("Conteúdos Inscritos " + devCarlos.getNome() + ": " + devCarlos.getConteudosInscritos());
+        System.out.println("Conteúdos Concluídos " + devCarlos.getNome() + ": " + devCarlos.getConteudosConcluidos());
+        System.out.println("XP: " + devValdir.calcularTotalXp());
 
     }
 
